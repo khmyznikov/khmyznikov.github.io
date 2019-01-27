62ac41e8e7f53bc202dba93bf1d9999c02d624b4
Khmyznikov Gleb <khmyznikov@gmail.com>
2019-01-27 21:02:59 +0300

    Fix main animation, add backward animation

--- invite/invite.js
+++ invite/invite.js
@@ -1,3 +1,14 @@
+var _opened = false;
 function openEnvelope() {
-    document.body.classList.toggle('animate');
+    if (!_opened){
+        document.body.classList.remove('reverse');
+        document.body.classList.add('animate');
+        _opened = true;
+    } else{
+        document.body.classList.remove('animate');
+        void document.body.offsetWidth;
+        document.body.classList.add('reverse');
+        _opened = false;
+    }
+    
 }
